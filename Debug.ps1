36410454fe51324ae744162f06e1512f479e6fb5
Maciej Chmiel <maciejowski2006yt@gmail.com>
2024-04-05 16:50:33 +0200

    Added parameter that tells PowerShell to ignore errors

--- Debug.ps1
+++ Debug.ps1
@@ -1,6 +1,6 @@
 Get-Process -Name "LocalAdmin" -ErrorAction Ignore | Stop-Process
 
-Remove-Item -Path "C:\Users\Maciej\AppData\Roaming\SCP Secret Laboratory\PluginAPI\plugins\global\ChaosMod3.dll" -Force 
+Remove-Item -Path "C:\Users\Maciej\AppData\Roaming\SCP Secret Laboratory\PluginAPI\plugins\global\ChaosMod3.dll" -Force -ErrorAction Ignore
 Copy-Item -Path "C:\Users\Maciej\Dev\ChaosMod3\ChaosMod3\bin\Release\ChaosMod3.dll" -Destination "C:\Users\Maciej\AppData\Roaming\SCP Secret Laboratory\PluginAPI\plugins\global\ChaosMod3.dll" 
 
 Start-Process -FilePath "P:\Serwery\SteamCMD\steamapps\common\SCP Secret Laboratory Dedicated Server\LocalAdmin.exe" -WorkingDirectory "P:\Serwery\SteamCMD\steamapps\common\SCP Secret Laboratory Dedicated Server" -ArgumentList "7777"
